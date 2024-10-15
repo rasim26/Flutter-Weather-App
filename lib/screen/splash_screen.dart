f3d9dbfc042ddc6c60cd1cb220a02f937323be57
Muhammed Rasim <muhammedrasim26@gmail.com>
2024-10-15 14:44:36 +0530

    splash screen updated

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,19 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/screen/hoome_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Get Real-Time Updates \n For Any City !'),
-          ],
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    var d = Duration(seconds: 4);
+    Future.delayed(d, () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (ctx) => ScreenHome(),
         ),
-      ), 
+      );
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(
+                'weather_app/Images/image.png',
+              ),
+              fit: BoxFit.fill),
+        ),
+      ),
     );
   }
 }
